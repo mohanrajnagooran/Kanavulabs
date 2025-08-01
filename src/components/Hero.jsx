4ab38161af2c816feaafba3c5211852767ccf017
mohanrajnagooran <77579188+mohanrajnagooran@users.noreply.github.com>
2025-08-01 17:19:56 +0530

    Updated footer responsiveness and other fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import Heroimage from '../assets/image 58.png'
 const Hero = () => {
   return (
     <section className="pt-2 pb-20 px-6 text-center bg-white">
-      <h1 className="text-4xl md:text-5xl font-bold mb-6">Innovate Without Limits</h1>
-      <p className="max-w-2xl mx-auto text-gray-600 mb-8">
+      <h1 className="text-4xl md:text-7xl font-bold mb-6">Innovate Without Limits</h1>
+      <p className="max-w-3xl mx-auto text-gray-600 mb-8 text-xl">
         Our passionate team crafts innovative solutions for complex business challenges. We partner for success through digital products, data-driven marketing, and expert problem-solving.
       </p>
       <div className="flex justify-center gap-4">
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
-import service1 from '../assets/service1.png'
-import service2 from '../assets/service2.png'
+import service1 from '../assets/service1.png';
+import service2 from '../assets/service2.png';
 
 const Articles = () => {
   const articles = [
@@ -12,7 +12,7 @@ const Articles = () => {
     },
     {
       title: "Software Development",
-      content: "We go beyond basic optimization, meticulously refining your content and technical foundation. This ensures search engines effortlessly find and showcase your business, driving highly relevant potential customers directly to you. Stop competing for visibility; start dominating it. ",
+      content: "We go beyond basic optimization, meticulously refining your content and technical foundation. This ensures search engines effortlessly find and showcase your business, driving highly relevant potential customers directly to you. Stop competing for visibility; start dominating it.",
       image: service2,
     },
     {
@@ -22,7 +22,7 @@ const Articles = () => {
     },
     {
       title: "Software Development",
-      content: "We go beyond basic optimization, meticulously refining your content and technical foundation. This ensures search engines effortlessly find and showcase your business, driving highly relevant potential customers directly to you. Stop competing for visibility; start dominating it. ",
+      content: "We go beyond basic optimization, meticulously refining your content and technical foundation. This ensures search engines effortlessly find and showcase your business, driving highly relevant potential customers directly to you. Stop competing for visibility; start dominating it.",
       image: service2,
     },
   ];
@@ -43,22 +43,23 @@ const Articles = () => {
   };
 
   return (
-    <section className="px-6 py-6 ">
-      <div className="max-w-7xl mx-auto p-2">
-        <div className="flex justify-between mb-6  mt-4">
-            {/* Heading */}
-        <div className=" mb-10">
+    <section className="px-6 py-12">
+      <div className="max-w-7xl mx-auto px-6">
+        
+        {/* Heading + Navigation Buttons */}
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-10">
+          {/* Heading */}
           <h2 className="text-2xl md:text-4xl font-bold">
             We are experts on all technologies & platforms
           </h2>
-        </div>
-        {/* Navigation Buttons */}
-        <div className="flex justify-center gap-4">
+
+          {/* Navigation Buttons */}
+          <div className="flex gap-4">
             <button
               onClick={prev}
               disabled={current === 0}
-              className={`p-3 rounded-full border border-gray-300 hover:bg-gray-100 transition ${
-                current === 0 ? "opacity-40 cursor-not-allowed" : ""
+              className={`p-3 rounded-md border border-black transition ${
+                current === 0 ? "opacity-100 cursor-not-allowed" : "text-white bg-black"
               }`}
             >
               <FaArrowLeft />
@@ -66,47 +67,41 @@ const Articles = () => {
             <button
               onClick={next}
               disabled={current + visibleCount >= articles.length}
-              className={`p-3 rounded-full border border-gray-300 hover:bg-gray-100 transition ${
-                current + visibleCount >= articles.length ? "opacity-40 cursor-not-allowed" : ""
+              className={`p-3 rounded-md border border-black transition ${
+                current + visibleCount >= articles.length ? "opacity-100 cursor-not-allowed" : "text-white bg-black"
               }`}
             >
               <FaArrowRight />
             </button>
-        </div>
+          </div>
         </div>
 
         {/* Article Cards */}
-        <div className="relative">
-          <div className="grid md:grid-cols-2 gap-6 mb-8">
-            {articles.slice(current, current + visibleCount).map((article, index) => (
-              <div
-                key={index}
-                className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md overflow-hidden transition"
-              >
-                <img
-                  src={article.image}
-                  alt={article.title}
-                  className="w-full h-60  object-contain"
-                />
-                <div className="p-2 mt-2">
-                  <h3 className="text-2xl font-bold mb-2">{article.title}</h3>
-                  <div className="flex justify-between ">
-                    <p className=" text-gray-500 text-sm mb-1">
-                        {article.content}
-                    </p>
-                  </div>
-                </div>
+        <div className="grid md:grid-cols-2 gap-6 mb-12">
+          {articles.slice(current, current + visibleCount).map((article, index) => (
+            <div
+              key={index}
+              className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md overflow-hidden transition"
+            >
+              <img
+                src={article.image}
+                alt={article.title}
+                className="w-full h-60 object-contain"
+              />
+              <div className="p-4">
+                <h3 className="text-2xl font-bold mb-2">{article.title}</h3>
+                <p className="text-gray-600 text-base">{article.content}</p>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </div>
-      <div className="items-center justify-center flex">
-        <div className="items-center justify-center">
-            <button className="border-black p-4 rounded-md border-2 hover:text-orange-400 hover:border-orange-400">
-                <a href="#" className="text-2xl font-semibold">Browse more Services</a>
-            </button>
-          </div>
+
+        {/* Browse More Button */}
+        <div className="flex justify-center">
+          <button className="border-black px-6 py-3 rounded-md border-2 hover:text-orange-400 hover:border-orange-400 transition text-lg font-semibold">
+            <a href="#">Browse more Services</a>
+          </button>
+        </div>
       </div>
     </section>
   );
