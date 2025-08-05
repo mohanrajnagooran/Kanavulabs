6592f7d950cd34e6cfc397cb3c823292bbfbcd68
mohanrajnagooran <77579188+mohanrajnagooran@users.noreply.github.com>
2025-08-05 20:41:03 +0530

    changing code for contact us button background color

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Careershome from '../components/careershome'
 
 
 const Careers = () => {
   return (
     <div className="md:pt-45 pt-30">
       <Navbar/>
-      <Careershome/>
+
       <Footer/>
     </div>
   )
